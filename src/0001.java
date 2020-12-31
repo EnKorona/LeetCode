de0539578f3ba7654793ae52ac3b62daf279460b
EnKorona <enkorona@gmail.com>
2020-12-31 14:31:14 +0800

    ac0001

--- src/0001.java
+++ src/0001.java
@@ -12,12 +12,20 @@
 //所以返回 [0, 1]
 //
 // Related Topics 数组 哈希表
-// 👍 9846 👎 0
+// 👍 9932 👎 0
 
 
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i<nums.length; i++) {
+            for (int j = i+1;j<nums.length;j++) {
+                if(nums[i]+nums[j] == target) {
+                    return new int[] {i,j};
+                }
+            }
+        }
+        return null;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
