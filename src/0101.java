7029f40af6ae4c0336822743892347183014f9e4
EnKorona <enkorona@gmail.com>
2021-01-23 17:01:01 +0800

    ac0101

---
+++ src/0101.java
@@ -0,0 +1,85 @@
+//给定一个二叉树，检查它是否是镜像对称的。
+//
+//
+//
+// 例如，二叉树 [1,2,2,3,4,4,3] 是对称的。
+//
+//     1
+//   / \
+//  2   2
+// / \ / \
+//3  4 4  3
+//
+//
+//
+//
+// 但是下面这个 [1,2,2,null,3,null,3] 则不是镜像对称的:
+//
+//     1
+//   / \
+//  2   2
+//   \   \
+//   3    3
+//
+//
+//
+//
+// 进阶：
+//
+// 你可以运用递归和迭代两种方法解决这个问题吗？
+// Related Topics 树 深度优先搜索 广度优先搜索
+// 👍 1207 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        TreeNode swapTree = swapTree(root);
+        return isSameTree(root, swapTree);
+    }
+
+    private TreeNode swapTree(TreeNode root) {
+        if (root == null) {
+            return root;
+        }
+        TreeNode leftNode = root.left;
+        TreeNode rightNode = root.right;
+        root.left = rightNode;
+        root.right = leftNode;
+        swapTree(root.left);
+        swapTree(root.right);
+        return root;
+    }
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        // 判断某两棵二叉树是否相同：根节点相同，而且左右子树都相同
+        // 判断根节点是否相同
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p == null || q == null) {
+            return false;
+        }
+        if (p.val != q.val) {
+            return false;
+        }
+        // 先序遍历左右子树是否相同，返回结果
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
