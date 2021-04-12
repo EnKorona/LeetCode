6d616c0e6372cecf6f9c0de93df514799d5a4373
fanziying <fanziying@jilinxiangyun.com>
2021-04-12 16:39:32 +0800

    ac 0104

---
+++ algorithms/cpp/l0104.cpp
@@ -0,0 +1,42 @@
+//给定一个二叉树，找出其最大深度。
+//
+// 二叉树的深度为根节点到最远叶子节点的最长路径上的节点数。
+//
+// 说明: 叶子节点是指没有子节点的节点。
+//
+// 示例：
+//给定二叉树 [3,9,20,null,null,15,7]，
+//
+//     3
+//   / \
+//  9  20
+//    /  \
+//   15   7
+//
+// 返回它的最大深度 3 。
+// Related Topics 树 深度优先搜索 递归
+// 👍 850 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxDepth(TreeNode *root) {
+        if (root == NULL) return 0;
+        int leftMaxDepth = maxDepth(root->left);
+        int rightMaxDepth = maxDepth(root->right);
+        return 1 + (leftMaxDepth > rightMaxDepth ? leftMaxDepth : rightMaxDepth);
+    }
+};
+//leetcode submit region end(Prohibit modification and deletion)
