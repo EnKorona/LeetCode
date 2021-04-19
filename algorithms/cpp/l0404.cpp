5d510aab860fc48899b49e33ed5f935fe3422f66
EnKorona <enkorona@gmail.com>
2021-04-19 09:58:01 +0800

    ac 0404

---
+++ algorithms/cpp/l0404.cpp
@@ -0,0 +1,45 @@
+//计算给定二叉树的所有左叶子之和。
+//
+// 示例：
+//
+//
+//    3
+//   / \
+//  9  20
+//    /  \
+//   15   7
+//
+//在这个二叉树中，有两个左叶子，分别是 9 和 15，所以返回 24
+//
+//
+// Related Topics 树
+// 👍 304 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int sum = 0;
+    int sumOfLeftLeaves(TreeNode* root) {
+        getSum(root);
+        return sum;
+    }
+    void getSum(TreeNode* root) {
+        if (root == NULL) return;
+        if (root -> left != NULL && root -> left -> left == NULL && root -> left -> right == NULL) sum += root -> left -> val;
+        getSum(root -> left);
+        getSum(root -> right);
+    }
+};
+//leetcode submit region end(Prohibit modification and deletion)
