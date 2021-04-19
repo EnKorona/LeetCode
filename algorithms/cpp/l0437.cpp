a2a598116de6abd44d6a215a778e7d3a3f212de9
EnKorona <enkorona@gmail.com>
2021-04-19 13:37:56 +0800

    ac 0437

---
+++ algorithms/cpp/l0437.cpp
@@ -0,0 +1,60 @@
+//给定一个二叉树，它的每个结点都存放着一个整数值。
+//
+// 找出路径和等于给定数值的路径总数。
+//
+// 路径不需要从根节点开始，也不需要在叶子节点结束，但是路径方向必须是向下的（只能从父节点到子节点）。
+//
+// 二叉树不超过1000个节点，且节点数值范围是 [-1000000,1000000] 的整数。
+//
+// 示例：
+//
+// root = [10,5,-3,3,2,null,11,3,-2,null,1], sum = 8
+//
+//      10
+//     /  \
+//    5   -3
+//   / \    \
+//  3   2   11
+// / \   \
+//3  -2   1
+//
+//返回 3。和等于 8 的路径有:
+//
+//1.  5 -> 3
+//2.  5 -> 2 -> 1
+//3.  -3 -> 11
+//
+// Related Topics 树
+// 👍 826 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int num = 0;
+    int pathSum(TreeNode* root, int targetSum) {
+        if (root == NULL) return 0;
+        getNum(root,targetSum);
+        pathSum(root -> left,targetSum);
+        pathSum(root -> right,targetSum);
+        return num;
+    }
+    void getNum(TreeNode* root, int targetSum) {
+        if (root == NULL) return;
+        if (root -> val == targetSum) num++;
+        getNum(root -> left,targetSum - root -> val);
+        getNum(root -> right,targetSum -root -> val);
+    }
+};
+//leetcode submit region end(Prohibit modification and deletion)
