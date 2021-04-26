1cb0bbc645388261b08c764b7a79d15e7eef1a86
EnKorona <enkorona@gmail.com>
2021-04-26 16:01:52 +0800

    ac l113

---
+++ algorithms/cpp/树/l0113.cpp
@@ -0,0 +1,72 @@
+//给你二叉树的根节点 root 和一个表示目标和的整数 targetSum ，判断该树中是否存在 根节点到叶子节点 的路径，这条路径上所有节点值相加等于目标和
+// targetSum 。
+//
+// 叶子节点 是指没有子节点的节点。
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：root = [5,4,8,11,null,13,4,7,2,null,null,null,1], targetSum = 22
+//输出：true
+//
+//
+// 示例 2：
+//
+//
+//输入：root = [1,2,3], targetSum = 5
+//输出：false
+//
+//
+// 示例 3：
+//
+//
+//输入：root = [1,2], targetSum = 0
+//输出：false
+//
+//
+//
+//
+// 提示：
+//
+//
+// 树中节点的数目在范围 [0, 5000] 内
+// -1000 <= Node.val <= 1000
+// -1000 <= targetSum <= 1000
+//
+// Related Topics 树 深度优先搜索
+// 👍 566 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> ans;
+    vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
+        vector<int> single;
+        getPath(single,root,targetSum);
+        return ans;
+    }
+    void getPath(vector<int> single, TreeNode* root, int targetSum) {
+        if (root == NULL) return;
+        single.push_back(root -> val);
+        if (root -> left == NULL && root -> right == NULL && root -> val == targetSum) {
+            ans.push_back(single);
+        }
+        getPath(single, root -> left,targetSum - root -> val);
+        getPath(single, root -> right,targetSum - root -> val);
+    }
+};
+//leetcode submit region end(Prohibit modification and deletion)
